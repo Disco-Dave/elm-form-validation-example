fb2ae5ff7fd8c496054dcb9bd77e8b53686adb23
David Burkett <13578196+Disco-Dave@users.noreply.github.com>
2022-03-14 21:04:06 -0400

    Added traverse functions

--- src/Utils/State.elm
+++ src/Utils/State.elm
@@ -6,8 +6,18 @@ module Utils.State exposing
     , applyMaybe
     , map
     , mapMaybe
+    , pure
+    , pureMaybe
     , run
+    , traverse
+    , traverseDict
+    , traverseDictMaybe
+    , traverseMaybe
     )
+
+import Dict exposing (Dict)
+
+
 
 -- ORIGINAL
 
@@ -30,6 +40,11 @@ map transform fromValue state =
     ( s1, transform f )
 
 
+pure : value -> State state value
+pure value state =
+    ( state, value )
+
+
 apply : State state fromValue -> State state (fromValue -> toValue) -> State state toValue
 apply fromValue transform state =
     let
@@ -40,6 +55,26 @@ apply fromValue transform state =
             fromValue s1
     in
     ( s2, t f )
+
+
+traverse : (fromValue -> State state toValue) -> List fromValue -> State state (List toValue)
+traverse transform inputs =
+    let
+        runAll input currentState =
+            map (::) (transform input)
+                |> apply currentState
+    in
+    List.foldr runAll (pure []) inputs
+
+
+traverseDict : (fromValue -> State state toValue) -> Dict comparable fromValue -> State state (Dict comparable toValue)
+traverseDict transform inputs =
+    let
+        runAll key input currentState =
+            map (Dict.insert key) (transform input)
+                |> apply currentState
+    in
+    Dict.foldr runAll (pure Dict.empty) inputs
 
 
 andThen : (fromValue -> State state toValue) -> State state fromValue -> State state toValue
@@ -68,6 +103,11 @@ mapMaybe transform fromValue =
     map (Maybe.map transform) fromValue
 
 
+pureMaybe : value -> StateMaybe state value
+pureMaybe =
+    pure << Just
+
+
 applyMaybe : StateMaybe state fromValue -> StateMaybe state (fromValue -> toValue) -> StateMaybe state toValue
 applyMaybe fromValue transform =
     let
@@ -80,3 +120,23 @@ applyMaybe fromValue transform =
                     always Nothing
     in
     apply fromValue (map transformMaybe transform)
+
+
+traverseMaybe : (fromValue -> StateMaybe state toValue) -> List fromValue -> StateMaybe state (List toValue)
+traverseMaybe transform inputs =
+    let
+        runAll input currentState =
+            mapMaybe (::) (transform input)
+                |> applyMaybe currentState
+    in
+    List.foldr runAll (pureMaybe []) inputs
+
+
+traverseDictMaybe : (fromValue -> StateMaybe state toValue) -> Dict comparable fromValue -> StateMaybe state (Dict comparable toValue)
+traverseDictMaybe transform inputs =
+    let
+        runAll key input currentState =
+            mapMaybe (Dict.insert key) (transform input)
+                |> applyMaybe currentState
+    in
+    Dict.foldr runAll (pureMaybe Dict.empty) inputs
